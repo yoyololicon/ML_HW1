54f1016e40ee878cb75139fec4ffb8d9d2435ce1
yoyololicon <ya70201@gmail.com>
2017-10-16 17:42:53 +0800

    update the numpy approach.

--- Using_numpy.py
+++ Using_numpy.py
@@ -3,19 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    data = pd.read_csv('data/4_test.csv')
+    test_data = pd.read_csv('data/4_test.csv')
+    train_data = pd.read_csv('data/4_train.csv')
 
-    t = np.array(data['t'].values)
-    x = np.array(data['x'].values)
+    train_t = np.array(train_data['t'].values)
+    test_t = np.array(test_data['t'].values)
+    train_x = np.array(train_data['x'].values)
+    test_x = np.array(test_data['x'].values)
 
-    w = np.polyfit(x, t, 2)
-    p = np.poly1d(w)
-    y = p(x)
-    rms = np.sqrt(np.sum((t-y)**2)/len(t))
+    train_elist = []
+    test_elist = []
 
-    print rms
+    for order in range(0, 10):
+        w = np.polyfit(train_x, train_t, order)
+        p = np.poly1d(w)
 
-    xp = np.linspace(0, 10, 100)
-    plt.plot(x, t, '.', xp, p(xp), '-')
-    plt.ylim(-5, 10)
+        train_y = p(train_x)
+        test_y = p(test_x)
+
+        train_elist.append(np.sqrt(np.mean((train_y - train_t) ** 2)))
+        test_elist.append(np.sqrt(np.mean((test_y - test_t) ** 2)))
+
+    plt.plot(train_elist, marker='o')
+    plt.plot(test_elist, 'r-', marker='o')
+    plt.xlim(-1, 10)
+    plt.xlabel('M')
+    plt.ylabel('RMS Error')
     plt.show()
